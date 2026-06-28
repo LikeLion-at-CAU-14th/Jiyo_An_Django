745d4dbd6e2b007338a984bc07994a51ca51caca
jayanjiyo <jay.anjiyo@gmail.com>
2026-06-28 19:33:05 +0900

    fix:prevent S3 file overwrite

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,7 @@
 ### DRF 관련 import - APIView 사용
+import os
+import uuid
+
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
@@ -113,44 +116,6 @@ class CommentListCreateView(APIView):
 from .serializers import ImageSerializer
 from django.conf import settings
 import boto3
-class ImageUploadView(APIView):
-    def post(self, request):
-        if 'image' not in request.FILES:
-            return Response({"error": "No image file"}, status=status.HTTP_400_BAD_REQUEST)
-
-        image_file = request.FILES['image']
-
-        s3_client = boto3.client(
-            "s3",
-            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-            region_name=settings.AWS_REGION
-        )
-
-        # S3에 파일 저장
-        file_path = f"uploads/{image_file.name}"
-        # S3에 파일 업로드
-        try:
-            s3_client.put_object(
-                Bucket=settings.AWS_STORAGE_BUCKET_NAME,
-                Key=file_path,
-                Body=image_file.read(),
-                ContentType=image_file.content_type,
-            )
-        except Exception as e:
-            return Response({"error": f"S3 Upload Failed: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-        # 업로드된 파일의 URL 생성
-        image_url = f"https://{settings.AWS_S3_CUSTOM_DOMAIN}/{file_path}"
-        
-        # DB에 저장
-        image_instance = Image.objects.create(image_url=image_url)
-        serializer = ImageSerializer(image_instance)
-
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
-
 """
 from django.shortcuts import render
 from django.http import JsonResponse
